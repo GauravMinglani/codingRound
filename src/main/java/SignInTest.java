eae3ca32e351582e98e6ee2726e3f718ea1e1c3b
GauravMinglani <39492085+GauravMinglani@users.noreply.github.com>
2018-05-23 22:53:26 +0530

    Update SignInTest.java
    Updated this file

--- src/main/java/SignInTest.java
+++ src/main/java/SignInTest.java
@@ -1,30 +1,64 @@
 import com.sun.javafx.PlatformUtil;
 import org.openqa.selenium.By;
+import org.openqa.selenium.By.ById;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class SignInTest {
+	
+	WebDriver driver;
+	WebElement element;
+	WebDriverWait wait;
 
-    WebDriver driver = new ChromeDriver();
+	@BeforeSuite(alwaysRun = true)
+	public void setupSuite() {
+		driver = new ChromeDriver();
+	}
+
+	@AfterSuite(alwaysRun = true)
+	public void tearDown() {
+		driver.quit();
+	}
+
+	@BeforeMethod(description = "maximize and open the window")
+	public void open() {
+		driver.manage().window().maximize();
+		driver.get("https://www.cleartrip.com/");
+	}
+
+
+//    WebDriver driver = new ChromeDriver();
 
     @Test
     public void shouldThrowAnErrorIfSignInDetailsAreMissing() {
 
-        setDriverPath();
+//        setDriverPath();
 
-        driver.get("https://www.cleartrip.com/");
-        waitFor(2000);
+//        driver.get("https://www.cleartrip.com/");
+//        waitFor(2000);
 
         driver.findElement(By.linkText("Your trips")).click();
         driver.findElement(By.id("SignIn")).click();
-
+        
+//        driver.switchTo().frame("modal_window");
+        driver.switchTo().frame(driver.findElement(By.id("modal_window")));
+        
+        waitFor(5000);
         driver.findElement(By.id("signInButton")).click();
 
         String errors1 = driver.findElement(By.id("errors1")).getText();
+        driver.switchTo().defaultContent();
+        driver.findElement(By.id("close")).click();
         Assert.assertTrue(errors1.contains("There were errors in your submission"));
-        driver.quit();
+//        driver.quit();
     }
 
     private void waitFor(int durationInMilliSeconds) {
@@ -35,17 +69,18 @@ public class SignInTest {
         }
     }
 
-    private void setDriverPath() {
-        if (PlatformUtil.isMac()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
-        }
-        if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        }
-        if (PlatformUtil.isLinux()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
-        }
-    }
+	@BeforeTest(description = "set type of driver which needs to be intialized")
+	private void setDriverPath() {
+		if (PlatformUtil.isMac()) {
+			System.setProperty("webdriver.chrome.driver", "chromedriver");
+		}
+		if (PlatformUtil.isWindows()) {
+			System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		}
+		if (PlatformUtil.isLinux()) {
+			System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
+		}
+	}
 
 
 }
