549518968d233d77417b91257c307a8573c1d22d
GauravMinglani <39492085+GauravMinglani@users.noreply.github.com>
2018-05-23 23:01:18 +0530

    Update HotelBookingTest.java
    Updated this file

--- src/main/java/HotelBookingTest.java
+++ src/main/java/HotelBookingTest.java
@@ -1,17 +1,50 @@
 import com.sun.javafx.PlatformUtil;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class HotelBookingTest {
+	
+	WebDriver driver;
+	WebElement element;
+	WebDriverWait wait;
 
-    WebDriver driver = new ChromeDriver();
+	@BeforeSuite(alwaysRun = true)
+	public void setupSuite() {
+		driver = new ChromeDriver();
+	}
 
-    @FindBy(linkText = "Hotels")
-    private WebElement hotelLink;
+	@AfterSuite(alwaysRun = true)
+	public void tearDown() {
+		driver.quit();
+	}
+
+	@BeforeMethod(description = "maximize and open the window")
+	public void open() {
+		driver.manage().window().maximize();
+		driver.get("https://www.cleartrip.com/");
+	}
+
+
+//    WebDriver driver = new ChromeDriver();
+
+//    @FindBy(xpath=".//*[@id='Home']/section/div/aside[1]/nav/ul[1]/li[2]/a[1]")
+	//@FindBy(css = "a[href='/hotels']")
+ //   WebElement hotelLink;
+//	@FindBy(css = "a[title='Find hotels in destinations around the world']")
+     
+	@FindBy(name= "localsNav")
+	WebElement local;
 
     @FindBy(id = "Tags")
     private WebElement localityTextBox;
@@ -24,30 +57,40 @@ public class HotelBookingTest {
 
     @Test
     public void shouldBeAbleToSearchForHotels() {
-        setDriverPath();
+//        setDriverPath();
 
-        driver.get("https://www.cleartrip.com/");
-        hotelLink.click();
+    	//WebElement web=driver.findElement(By.xpath(".//*[@id='Home']/section/div/aside[1]/nav/ul[1]/li[2]/a[1]"));
+    	
+//    	WebElement web=driver.findElement(By.xpath("/html/body/section[2]/section/div/aside[1]/nav/ul[1]/li[2]/a[1]"));
+    	
+    	///html/body/section[2]/section/div/aside[1]/nav/ul[1]/li[2]/a[1]
+//        driver.get("https://www.cleartrip.com/");
+//    	System.out.println("value name"+web.getText());
+    	System.out.println("FOund");
+        /*hotelLink.click();*/
+    	
+    	local.click();
 
         localityTextBox.sendKeys("Indiranagar, Bangalore");
 
         new Select(travellerSelection).selectByVisibleText("1 room, 2 adults");
         searchButton.click();
 
-        driver.quit();
+//        driver.quit();
 
     }
 
-    private void setDriverPath() {
-        if (PlatformUtil.isMac()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
-        }
-        if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        }
-        if (PlatformUtil.isLinux()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
-        }
-    }
+	@BeforeTest(description = "set type of driver which needs to be intialized")
+	private void setDriverPath() {
+		if (PlatformUtil.isMac()) {
+			System.setProperty("webdriver.chrome.driver", "chromedriver");
+		}
+		if (PlatformUtil.isWindows()) {
+			System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		}
+		if (PlatformUtil.isLinux()) {
+			System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
+		}
+	}
 
 }
